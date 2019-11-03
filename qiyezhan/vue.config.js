49108096000155c1991feb50956004a9d90c2d1f
学习的小王 <807515004@qq.com>
2019-11-03 16:28:59 +0800

    test2

--- qiyezhan/vue.config.js
+++ qiyezhan/vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
             }
         }
     },
-    // publicPath:'./',
+    publicPath:'./',
     lintOnSave: false,//关闭格式检查
     productionSourceMap: false,//打包时不会生成.map文件，加快打包速度
 }
